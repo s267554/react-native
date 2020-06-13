444733a1583de817dfd5a8bf3f731a793645ce29
Paolo Destefanis <s257424@studenti.polito.it>
2020-06-13 10:18:32 +0200

    buttons style

--- components/Home/index.js
+++ components/Home/index.js
@@ -6,6 +6,7 @@ import Autocomplete from 'react-native-autocomplete-input';
 import { ScrollView } from 'react-native-gesture-handler';
 import IconMaterialCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
 import Connection from '../Connection';
+import IconMaterial from 'react-native-vector-icons/MaterialIcons';
 
 
 function Home({ navigation }) {
@@ -88,9 +89,20 @@ function Home({ navigation }) {
         });
     }
 
+    const cancelTravels = () => {
+        setDate(new Date())
+        setTravels([])
+        setStartQuery("")
+        setEndQuery("")
+        setStartStationId(undefined)
+        setEndStationId(undefined)
+        setStartStations([])
+        setEndStations([])
+    }
+
     const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
     return (
-        <ScrollView 
+        <ScrollView
             keyboardShouldPersistTaps='handled'
             style={{
                 backgroundColor: '#EBEDEF',
@@ -101,197 +113,201 @@ function Home({ navigation }) {
         >
             <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
             <View style={styles.searchContainer}>
-            <View
-                style={{
-                    margin: 10,
-                    flexDirection: 'row',
-                }}
-            >
-                <Image
-                    style={{
-                        width: 24,
-                        height: 24,
-                        marginHorizontal: 8
-                    }}
-                    source={suisseIcon}
-                />
-                <Text
-                    style={styles.headText}
-                >Switzerland travel system</Text>
-            </View>
-            <SafeAreaView
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'stretch',
-                    padding: 20,
-                }}
-            >
-                <View style={styles.container}>
-                    
-                     <IconMaterialCommunity
-                         name = "arrow-top-right"
-                         size = {40}
-                     />
-                    <Autocomplete
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        containerStyle={styles.autocompleteContainer}
-                        inputContainerStyle={{borderWidth: 0}}
-                        data={startFlag ? [] : startStations}
-                        onFocus={() => setStartFlag(false)}
-                        onBlur={() => setStartFlag(true)}
-                        defaultValue={startQuery}
-                        onChangeText={text => setStartQuery(text)}
-                        placeholder="From:"
-                        renderItem={(item) => (
-                            <TouchableOpacity onPress={() => {
-                                Keyboard.dismiss();
-                                setStartFlag(true);
-                                setStartQuery(item.item.name)
-                                setStartStationId(item.item.id)
-                            }}>
-                                <Text style={styles.itemText}>
-                                    {item.item.name}
-                                </Text>
-                            </TouchableOpacity>
-                        )}
-                    />
-                </View>
-                <TouchableOpacity
-                    onPress={() => { 
-                        const [a, b] = [endQuery, startQuery]; setStartQuery(a); setEndQuery(b)
-                        const [c, d] = [endStationId, startStationId]; setStartStationId(c); setEndStationId(d) 
-                    }}
-                >
-                    <IconMaterialCommunity
-                        name = "swap-vertical"
-                        size = {40}
-                    />
-                </TouchableOpacity>
-                <View style={styles.container}>
-                    <IconMaterialCommunity
-                        name = "arrow-bottom-right"
-                        size = {40}
-                    />
-                    <Autocomplete
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        containerStyle={styles.autocompleteContainer}
-                        inputContainerStyle={{borderWidth: 0}}
-                        data={endFlag ? [] : endStations}
-                        onFocus={() => setEndFlag(false)}
-                        onBlur={() => setEndFlag(true)}
-                        defaultValue={endQuery}
-                        onChangeText={text => setEndQuery(text)}
-                        placeholder="To:"
-                        renderItem={(item) => (
-                            <TouchableOpacity onPress={() => {
-                                Keyboard.dismiss();
-                                setEndFlag(true);
-                                setEndQuery(item.item.name)
-                                setEndStationId(item.item.id)
-                            }}>
-                                <Text style={styles.itemText}>
-                                    {item.item.name}
-                                </Text>
-                            </TouchableOpacity>
-                        )}
-                    />
-                </View>
                 <View
-                    style={{ 
-                        flexDirection: "row",
-                        marginBottom: 20
+                    style={{
+                        margin: 10,
+                        flexDirection: 'row',
                     }}
                 >
-                    <TouchableOpacity
-                        onPress={showDatepicker}
-                        style={styles.textInputContainer}
-                    >
+                    <Image
+                        style={{
+                            width: 24,
+                            height: 24,
+                            marginHorizontal: 8
+                        }}
+                        source={suisseIcon}
+                    />
+                    <Text
+                        style={styles.headText}
+                    >Switzerland travel system</Text>
+                </View>
+                <SafeAreaView
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'stretch',
+                        padding: 20,
+                    }}
+                >
+                    <View style={styles.container}>
+
                         <IconMaterialCommunity
-                            name="calendar"
+                            name="arrow-top-right"
                             size={40}
                         />
-                        <Text
-                            placeholder="Date:"
-                        >
-                            {date.toDateString().split(' ').slice(0, 3).join(' ')}
-                        </Text>
-                    </TouchableOpacity>
+                        <Autocomplete
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            containerStyle={styles.autocompleteContainer}
+                            inputContainerStyle={{ borderWidth: 0 }}
+                            data={startFlag ? [] : startStations}
+                            onFocus={() => setStartFlag(false)}
+                            onBlur={() => setStartFlag(true)}
+                            defaultValue={startQuery}
+                            onChangeText={text => setStartQuery(text)}
+                            placeholder="From:"
+                            renderItem={(item) => (
+                                <TouchableOpacity onPress={() => {
+                                    Keyboard.dismiss();
+                                    setStartFlag(true);
+                                    setStartQuery(item.item.name)
+                                    setStartStationId(item.item.id)
+                                }}>
+                                    <Text style={styles.itemText}>
+                                        {item.item.name}
+                                    </Text>
+                                </TouchableOpacity>
+                            )}
+                        />
+                    </View>
                     <TouchableOpacity
-                        onPress={showTimepicker}
-                        style={styles.textInputContainer}
+                        onPress={() => {
+                            const [a, b] = [endQuery, startQuery]; setStartQuery(a); setEndQuery(b)
+                            const [c, d] = [endStationId, startStationId]; setStartStationId(c); setEndStationId(d)
+                        }}
                     >
-                        <Text
-                            placeholder="Time:"
-                        >
-                            {date.toLocaleTimeString().split(':').slice(0, 2).join(':')}
-                        </Text>
                         <IconMaterialCommunity
-                            name="clock"
+                            name="swap-vertical"
                             size={40}
                         />
                     </TouchableOpacity>
-                </View>
-                <View>
-                    {show && (
-                        <DateTimePicker
-                            testID="dateTimePicker"
-                            value={date}
-                            mode={mode}
-                            is24Hour={true}
-                            display="default"
-                            onChange={onChangeDateTime}
+                    <View style={styles.container}>
+                        <IconMaterialCommunity
+                            name="arrow-bottom-right"
+                            size={40}
                         />
-                    )}
-                </View>
-                <View>
-                    <Button
-                        title="Cerca"
-                        onPress={findTravels}
-                        disabled={startStationId == undefined || endStationId == undefined}                      
-                    />
-                </View>
-                {travels.map((travel) => {
-                    return <Connection connection={travel}/>
-                })}
-                <View>
-                    <Button
-                        title="Vai a Travel"
-                        onPress={() => navigation.navigate('Travel', { id: "ciao" })}
-                    />
-                </View>
-            </SafeAreaView>
+                        <Autocomplete
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            containerStyle={styles.autocompleteContainer}
+                            inputContainerStyle={{ borderWidth: 0 }}
+                            data={endFlag ? [] : endStations}
+                            onFocus={() => setEndFlag(false)}
+                            onBlur={() => setEndFlag(true)}
+                            defaultValue={endQuery}
+                            onChangeText={text => setEndQuery(text)}
+                            placeholder="To:"
+                            renderItem={(item) => (
+                                <TouchableOpacity onPress={() => {
+                                    Keyboard.dismiss();
+                                    setEndFlag(true);
+                                    setEndQuery(item.item.name)
+                                    setEndStationId(item.item.id)
+                                }}>
+                                    <Text style={styles.itemText}>
+                                        {item.item.name}
+                                    </Text>
+                                </TouchableOpacity>
+                            )}
+                        />
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            marginBottom: 20
+                        }}
+                    >
+                        <TouchableOpacity
+                            onPress={showDatepicker}
+                            style={styles.textInputContainer}
+                        >
+                            <IconMaterialCommunity
+                                name="calendar"
+                                size={40}
+                            />
+                            <Text
+                                placeholder="Date:"
+                            >
+                                {date.toDateString().split(' ').slice(0, 3).join(' ')}
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={showTimepicker}
+                            style={styles.textInputContainer}
+                        >
+                            <Text
+                                placeholder="Time:"
+                            >
+                                {date.toLocaleTimeString().split(':').slice(0, 2).join(':')}
+                            </Text>
+                            <IconMaterialCommunity
+                                name="clock"
+                                size={40}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        {show && (
+                            <DateTimePicker
+                                testID="dateTimePicker"
+                                value={date}
+                                mode={mode}
+                                is24Hour={true}
+                                display="default"
+                                onChange={onChangeDateTime}
+                            />
+                        )}
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-evenly'
+                        }}>
+                        <TouchableOpacity
+                            onPress={findTravels}
+                            style={startStationId == undefined || endStationId == undefined ? styles.searchButtonDisabled : styles.searchButton}
+                            disabled={startStationId == undefined || endStationId == undefined}
+                        >
+                            <IconMaterial
+                                name="search"
+                                size={30}
+                                color="white"
+                            />
+                            <Text style={styles.textButton}>SEARCH</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={cancelTravels}
+                            style={styles.cancelButton}
+                        >
+                            <IconMaterial
+                                name="cancel"
+                                color="white"
+                                size={30}
+                            />
+                            <Text style={styles.textButton}>CANCEL</Text>
+                        </TouchableOpacity>
+                    </View>
+                    {travels.map((travel) => {
+                        return <Connection connection={travel} />
+                    })}
+                </SafeAreaView>
             </View>
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    icon: { //not used
-        height: 24,
-        width: 24,
-        alignSelf: "flex-end"
-    },
     searchContainer: {
         margin: 15,
         backgroundColor: 'white',
         borderRadius: 10
     },
-    headText:{
+    headText: {
         fontFamily: 'Montserrat-Medium',
         fontWeight: "bold",
         fontSize: 20,
         lineHeight: 24,
-        letterSpacing: 0.1,
-    },
-    commonText:{ //not used
-        fontFamily: 'Montserrat-Medium',
-        fontSize: 16,
-        lineHeight: 24,
-        letterSpacing: 0.1,
-        margin: 5
+        letterSpacing: 0.2,
     },
     textInputContainer: {
         flexDirection: "row",
@@ -302,7 +318,6 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         paddingTop: 25,
-       // paddingTop: 25,
         paddingBottom: 35
     },
     autocompleteContainer: {
@@ -317,25 +332,42 @@ const styles = StyleSheet.create({
     },
     descriptionContainer: {
     },
-    infoText: { //not used
-        textAlign: 'center'
+    searchButton: {
+        borderRadius: 10,
+        margin: 10,
+        padding: 10,
+        backgroundColor: '#D52B1E',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: "center"
     },
-    titleText: { //not used
-        fontSize: 18,
-        fontWeight: '500',
-        marginBottom: 10,
-        marginTop: 10,
-        textAlign: 'center'
+    searchButtonDisabled: {
+        borderRadius: 10,
+        margin: 10,
+        padding: 10,
+        backgroundColor: '#BDBDBD',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: "center"
     },
-    directorText: { //not used
-        color: 'grey',
-        fontSize: 12,
-        marginBottom: 10,
-        textAlign: 'center'
+    cancelButton: {
+        borderRadius: 10,
+        margin: 10,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: "center",
+        backgroundColor: '#D52B1E',
     },
-    openingText: { //not used
-        textAlign: 'center'
-    },
+    textButton: {
+        fontFamily: 'Montserrat-Medium',
+        fontWeight: "bold",
+        fontSize: 15,
+        color: "white",
+        lineHeight: 24,
+        letterSpacing: 0.1,
+        marginHorizontal: 10
+    }
 });
 
 export default Home
